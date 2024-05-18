096ae1f0833579ed90dd3070d0ac662087b65603
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-18 02:44:02 +0200

    Add a script to format the whole SRC BUT UNSAFE FOR NOW + remove test scripts

---
+++ t_format_workspace.py
@@ -0,0 +1,29 @@
+import glob
+import os
+import subprocess
+import sys
+import threading
+
+def format_files(files):
+    blacklist = [""]
+    blacklist_dirs = ["ThirdPartyLibs", "Shaders","Engine","Project","Source"]
+    for file in files:
+        if os.path.basename(file) not in blacklist and not any(blacklist_dir in file for blacklist_dir in blacklist_dirs):
+            print(f"Formatting {file}...")
+            subprocess.run(["clang-format", "-i", "-style=file", file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            print(f"Skipping {file}...")
+
+if len(sys.argv) > 1:
+    print(f"formatting file(s) {sys.argv[1:]}")
+    format_thread = threading.Thread(target=format_files, args=(sys.argv[1:],))
+    format_thread.start()
+    format_thread.join()
+    print("done.")
+else:
+    print("formatting...")
+    files_to_format = glob.glob("**/*.[chin]", recursive=True)
+    format_thread = threading.Thread(target=format_files, args=(files_to_format,))
+    format_thread.start()
+    format_thread.join()
+    print("done.")

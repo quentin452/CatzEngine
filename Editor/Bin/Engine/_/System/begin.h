d7ef57302d39032adbd004cec6c167159fb812b8
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-19 17:21:26 +0200

    Format files from Editor\Bin\Engine\_\System\
     (+ fix cannot format without errors if there is)

--- Editor/Bin/Engine/_/System/begin.h
+++ Editor/Bin/Engine/_/System/begin.h
@@ -3,24 +3,25 @@
  * Titan Engine (https://esenthel.com) header file.                           *
 /******************************************************************************/
 #ifdef _WIN32 // WINDOWS
-   #define LOCK_WRITE WindowsLOCK_WRITE
-   #define Font       WindowsFont
-   #define FontPtr    WindowsFontPtr
+#define LOCK_WRITE WindowsLOCK_WRITE
+#define Font WindowsFont
+#define FontPtr WindowsFontPtr
 #elif defined __APPLE__ // APPLE
-   #define Ptr       ApplePtr
-   #define Point     ApplePoint
-   #define Cell      AppleCell
-   #define Rect      AppleRect
-   #define Button    AppleButton
-   #define Cursor    AppleCursor
-   #define FileInfo  AppleFileInfo
-   #define TextStyle AppleTextStyle
-#elif (defined __linux__ && !defined ANDROID) || defined EMSCRIPTEN // LINUX or WEB
-   #define Time   LinuxTime
-   #define Font   LinuxFont
-   #define Region LinuxRegion
-   #define Window XWindow
-   #define Cursor XCursor
+#define Ptr ApplePtr
+#define Point ApplePoint
+#define Cell AppleCell
+#define Rect AppleRect
+#define Button AppleButton
+#define Cursor AppleCursor
+#define FileInfo AppleFileInfo
+#define TextStyle AppleTextStyle
+#elif (defined __linux__ && !defined ANDROID) ||                               \
+    defined EMSCRIPTEN // LINUX or WEB
+#define Time LinuxTime
+#define Font LinuxFont
+#define Region LinuxRegion
+#define Window XWindow
+#define Cursor XCursor
 #elif defined ANDROID // ANDROID
 #endif
 /******************************************************************************/

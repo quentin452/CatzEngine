ed7d7fc931b440a60bf19352f22dc89efa03c304
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-19 17:28:17 +0200

    Format files from Editor\Bin\Engine\_\ (but not headers.h for now)
    (+ fix cannot format without errors if there is)

--- Editor/Bin/Engine/_/structs.h
+++ Editor/Bin/Engine/_/structs.h
@@ -153,35 +153,35 @@ struct BLST;
 /******************************************************************************/
 // GUI
 /******************************************************************************/
-struct  GUI;
-struct  GuiObj;
-struct  GuiObjs;
-struct  GuiObjChildren;
-struct  Button;
-struct  CheckBox;
-struct  ColorPicker;
-struct  ComboBox;
-struct  Dialog;
-struct  Desktop;
-struct  GuiCustom;
-struct  Text;
-struct  Viewport;
-struct  GuiImage;
+struct GUI;
+struct GuiObj;
+struct GuiObjs;
+struct GuiObjChildren;
+struct Button;
+struct CheckBox;
+struct ColorPicker;
+struct ComboBox;
+struct Dialog;
+struct Desktop;
+struct GuiCustom;
+struct Text;
+struct Viewport;
+struct GuiImage;
 struct _List;
-struct  ListColumn;
-struct  MenuElm;
-struct  Menu;
-struct  MenuBar;
-struct  Progress;
-struct  Region;
-struct  SlideBar;
-struct  Slider;
-struct  Tab;
-struct  Tabs;
-struct  TextBox;
-struct  TextLine;
-struct  Window;
-struct  WindowIO;
+struct ListColumn;
+struct MenuElm;
+struct Menu;
+struct MenuBar;
+struct Progress;
+struct Region;
+struct SlideBar;
+struct Slider;
+struct Tab;
+struct Tabs;
+struct TextBox;
+struct TextLine;
+struct Window;
+struct WindowIO;
 /******************************************************************************/
 // MESH
 /******************************************************************************/
@@ -233,23 +233,23 @@ struct UID;
 struct _Memc;
 struct _Memb;
 struct _Memx;
-struct  MemlNode;
+struct MemlNode;
 T1(TYPE) struct Mems;
 T1(TYPE) struct FixedMems;
 T1(TYPE) struct Memc;
 T1(TYPE) struct Memb;
 T1(TYPE) struct Memx;
 T1(TYPE) struct Meml;
-template<typename TYPE, Int size=64*1024>   struct  Memt;
-template<typename TYPE, Int size=64*1024>   struct CMemPtr;
-template<typename TYPE, Int size=64*1024>   struct  MemPtr;
+template <typename TYPE, Int size = 64 * 1024> struct Memt;
+template <typename TYPE, Int size = 64 * 1024> struct CMemPtr;
+template <typename TYPE, Int size = 64 * 1024> struct MemPtr;
 struct _Grid;
 struct _Map;
 struct _Cache;
-T1(TYPE    ) struct Cache;
-T2(KEY,DATA) struct MapEx;
-template<typename TYPE              , Cache<TYPE    > &CACHE>   struct CacheElmPtr;
-template<typename KEY, typename DATA, MapEx<KEY,DATA> &MAP  >   struct   MapElmPtr;
+T1(TYPE) struct Cache;
+T2(KEY, DATA) struct MapEx;
+template <typename TYPE, Cache<TYPE> &CACHE> struct CacheElmPtr;
+template <typename KEY, typename DATA, MapEx<KEY, DATA> &MAP> struct MapElmPtr;
 struct Object;
 struct DataCallback;
 struct Notification;
@@ -289,30 +289,28 @@ struct Socket;
 /******************************************************************************/
 // GAME
 /******************************************************************************/
-namespace Game
-{
-   struct Obj;
-   struct Item;
-   struct Chr;
-   struct Area;
-   struct WorldSettings;
-   struct WorldManager;
-   T1(TYPE) struct ObjMap;
-}
+namespace Game {
+struct Obj;
+struct Item;
+struct Chr;
+struct Area;
+struct WorldSettings;
+struct WorldManager;
+T1(TYPE) struct ObjMap;
+} // namespace Game
 /******************************************************************************/
 // EDIT
 /******************************************************************************/
-namespace Edit
-{
-   struct Symbol;
-   struct Line;
-   struct Source;
-   struct Token;
-   struct Macro;
-   struct Expr;
-   struct Command;
-   struct Message;
-   struct Compiler;
-   struct CompilerContext;
-}
+namespace Edit {
+struct Symbol;
+struct Line;
+struct Source;
+struct Token;
+struct Macro;
+struct Expr;
+struct Command;
+struct Message;
+struct Compiler;
+struct CompilerContext;
+} // namespace Edit
 /******************************************************************************/

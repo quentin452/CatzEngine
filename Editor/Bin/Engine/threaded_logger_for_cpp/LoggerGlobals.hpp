3e4749625252c10119e1e4a62b18493886af38b5
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-05-24 19:01:36 +0200

    format LoggerGlobals.hpp

--- Editor/Bin/Engine/threaded_logger_for_cpp/LoggerGlobals.hpp
+++ Editor/Bin/Engine/threaded_logger_for_cpp/LoggerGlobals.hpp
@@ -7,12 +7,11 @@
 #include <thread>
 
 class LoggerGlobals {
-public:
-  static inline std::string UsernameDirectory;
-  static inline std::string LogFilePath;
-  static inline std::string LogFolderPath;
-  static inline std::string LogFolderBackupPath;
-  static inline std::string LogFileBackupPath;
-  static inline std::string SrcProjectDirectory;
+  public:
+    static inline std::string UsernameDirectory;
+    static inline std::string LogFilePath;
+    static inline std::string LogFolderPath;
+    static inline std::string LogFolderBackupPath;
+    static inline std::string LogFileBackupPath;
+    static inline std::string SrcProjectDirectory;
 };
-

1ed861fff111514807d304ff39fd6db790164068
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-08-03 04:40:36 +0200

    Update Obj Particles.cpp

--- Engine/Source/Game/Objects/Obj Particles.cpp
+++ Engine/Source/Game/Objects/Obj Particles.cpp
@@ -14,7 +14,8 @@ void ObjParticles::create(Object &obj) {
     PROFILE_START("CatzEngine::ObjParticles::create(Object &obj)")
     arbitrary_name = "ObjParticle Object";
     Particles *ps = null;
-    if (Param *p = obj.findParam("particles")) // have Particles object specified
+    Param *p;
+    if (p = obj.findParam("particles")) // have Particles object specified
     {
         if (p->type == PARAM_STR)
             ps = ParticlesCache(p->asText());
@@ -26,144 +27,144 @@ void ObjParticles::create(Object &obj) {
         particles.create(*ps);
     else // create from particles
     {    // otherwise check for all parameters manually
-        if (Param *p = obj.findParam("Image")) {
+        if (p = obj.findParam("Image")) {
             if (p->type == PARAM_STR)
                 particles.image = p->asText();
             else if (p->type == PARAM_ID)
                 particles.image = p->asID();
         }
-        if (Param *p = obj.findParam("Palette Image")) {
+        if (p = obj.findParam("Palette Image")) {
             if (p->type == PARAM_STR)
                 particles.palette_image = p->asText();
             else if (p->type == PARAM_ID)
                 particles.palette_image = p->asID();
         }
-        if (Param *p = obj.findParam("Image Frames X"))
+        if (p = obj.findParam("Image Frames X"))
             particles.image_x_frames = Mid(p->asInt(), 1, 65535);
         else
             particles.image_x_frames = 1;
-        if (Param *p = obj.findParam("Image Frames Y"))
+        if (p = obj.findParam("Image Frames Y"))
             particles.image_y_frames = Mid(p->asInt(), 1, 65535);
         else
             particles.image_y_frames = 1;
-        if (Param *p = obj.findParam("Image Speed"))
+        if (p = obj.findParam("Image Speed"))
             particles.image_speed = p->asFlt();
         else
             particles.image_speed = 1;
-        if (Param *p = obj.findParam("Reborn"))
+        if (p = obj.findParam("Reborn"))
             particles.reborn = p->asBool();
         else
             particles.reborn = true;
-        if (Param *p = obj.findParam("Emitter Life"))
+        if (p = obj.findParam("Emitter Life"))
             particles.emitter_life_max = Max(p->asFlt(), 0);
         else
             particles.emitter_life_max = 0;
-        if (Param *p = obj.findParam("Fade In Time"))
+        if (p = obj.findParam("Fade In Time"))
             particles.fade_in = Max(p->asFlt(), 0);
         else
             particles.fade_in = 1;
-        if (Param *p = obj.findParam("Fade Out Time"))
+        if (p = obj.findParam("Fade Out Time"))
             particles.fade_out = Max(p->asFlt(), 0);
         else
             particles.fade_out = 1;
-        if (Param *p = obj.findParam("Palette Mode"))
+        if (p = obj.findParam("Palette Mode"))
             particles.palette(p->asBool());
         else
             particles.palette(true);
-        if (Param *p = obj.findParam("Palette Index"))
+        if (p = obj.findParam("Palette Index"))
             particles.paletteIndex(p->asBool());
         else
             particles.paletteIndex(false);
-        if (Param *p = obj.findParam("Smooth Fade"))
+        if (p = obj.findParam("Smooth Fade"))
             particles.smooth_fade = p->asBool();
         else
             particles.smooth_fade = false;
-        if (Param *p = obj.findParam("Elements"))
+        if (p = obj.findParam("Elements"))
             particles.p.setNumZero(Max(p->asInt(), 0));
         else
             particles.p.setNumZero(40);
-        if (Param *p = obj.findParam("Color"))
+        if (p = obj.findParam("Color"))
             particles.color = p->asColor();
         else
             particles.color.set(158, 0, 0, 0);
-        if (Param *p = obj.findParam("Glow"))
+        if (p = obj.findParam("Glow"))
             particles.glow = Mid(p->asInt(), 0, 255);
         else
             particles.glow = 0;
-        if (Param *p = obj.findParam("Radius"))
+        if (p = obj.findParam("Radius"))
             particles.radius = p->asFlt();
         else
             particles.radius = 0.048f;
-        if (Param *p = obj.findParam("Radius Random"))
+        if (p = obj.findParam("Radius Random"))
             particles.radius_random = Max(p->asFlt(), 0);
         else
             particles.radius_random = 0;
-        if (Param *p = obj.findParam("Radius Growth"))
+        if (p = obj.findParam("Radius Growth"))
             particles.radius_growth = Max(p->asFlt(), 0);
         else
             particles.radius_growth = 1;
-        if (Param *p = obj.findParam("Offset Range"))
+        if (p = obj.findParam("Offset Range"))
             particles.offset_range = p->asFlt();
         else
             particles.offset_range = 0;
-        if (Param *p = obj.findParam("Offset Speed"))
+        if (p = obj.findParam("Offset Speed"))
             particles.offset_speed = p->asFlt();
         else
             particles.offset_speed = 1;
-        if (Param *p = obj.findParam("Life"))
+        if (p = obj.findParam("Life"))
             particles.life = Max(p->asFlt(), 0);
         else
             particles.life = 1.31f;
-        if (Param *p = obj.findParam("Life Random"))
+        if (p = obj.findParam("Life Random"))
             particles.life_random = Max(p->asFlt(), 0);
         else
             particles.life_random = 0;
-        if (Param *p = obj.findParam("Glue"))
+        if (p = obj.findParam("Glue"))
             particles.glue = Sat(p->asFlt());
         else
             particles.glue = 0;
-        if (Param *p = obj.findParam("Velocity Damping"))
+        if (p = obj.findParam("Velocity Damping"))
             particles.damping = Sat(p->asFlt());
         else
             particles.damping = 0;
-        if (Param *p = obj.findParam("Angular Velocity"))
+        if (p = obj.findParam("Angular Velocity"))
             particles.ang_vel = p->asFlt();
         else
             particles.ang_vel = 0;
-        if (Param *p = obj.findParam("Random Velocity"))
+        if (p = obj.findParam("Random Velocity"))
             particles.vel_random = p->asFlt();
         else
             particles.vel_random = 0;
-        if (Param *p = obj.findParam("Initial Velocity X"))
+        if (p = obj.findParam("Initial Velocity X"))
             particles.vel_constant.x = p->asFlt();
         else
             particles.vel_constant.x = 0;
-        if (Param *p = obj.findParam("Initial Velocity Y"))
+        if (p = obj.findParam("Initial Velocity Y"))
             particles.vel_constant.y = p->asFlt();
         else
             particles.vel_constant.y = 0;
-        if (Param *p = obj.findParam("Initial Velocity Z"))
+        if (p = obj.findParam("Initial Velocity Z"))
             particles.vel_constant.z = p->asFlt();
         else
             particles.vel_constant.z = 0;
-        if (Param *p = obj.findParam("Acceleration X"))
+        if (p = obj.findParam("Acceleration X"))
             particles.accel.x = p->asFlt();
         else
             particles.accel.x = 0;
-        if (Param *p = obj.findParam("Acceleration Y"))
+        if (p = obj.findParam("Acceleration Y"))
             particles.accel.y = p->asFlt();
         else
             particles.accel.y = 0;
-        if (Param *p = obj.findParam("Acceleration Z"))
+        if (p = obj.findParam("Acceleration Z"))
             particles.accel.z = p->asFlt();
         else
             particles.accel.z = 0;
-        if (Param *p = obj.findParam("Inside Source"))
+        if (p = obj.findParam("Inside Source"))
             particles.inside_shape = p->asBool();
         else
             particles.inside_shape = true;
         SHAPE_TYPE shape_type = SHAPE_NONE;
-        if (Param *p = obj.findParam("Source Shape"))
+        if (p = obj.findParam("Source Shape"))
             shape_type = p->asEnum(SHAPE_NONE);
         switch (shape_type) {
         default:
